e61e95618d39a3ab58f2d11625aa56634ab81640
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-04-06 19:28:37 +0300

    'Fix_code_duplication'

--- app/create_file.py
+++ app/create_file.py
@@ -3,54 +3,46 @@ import os
 from datetime import datetime
 
 
-def create_file(filename: str, content: list[str]) -> None:
+def create_file(file_path: str) -> None:
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    mode = "a" if os.path.exists(filename) else "w"
+    mode = "a" if os.path.exists(file_path) else "w"
 
-    with open(filename, mode) as file:
-        if mode == "w":
-            file.write(timestamp + "\n")
+    with open(file_path, mode) as file:
+        file.write(timestamp + "\n")
 
-        for i, line in enumerate(content, start=1):
-            if mode == "a" and i == 1:
-                file.write(timestamp + "\n")
-            file.write(f"{i} {line}\n")
+        line_number = 1
+        while True:
+            line_content = input("Enter content line: ")
+            if line_content == "stop":
+                file.write("\n")
+                break
+            file.write(f"{line_number} {line_content}\n")
+            line_number += 1
 
 
 def main() -> None:
-    if len(sys.argv) < 3:
-        return
-
     if "-d" in sys.argv and "-f" in sys.argv:
         directory_index = sys.argv.index("-d") + 1
         filename_index = sys.argv.index("-f") + 1
+        if directory_index < filename_index:
+            directory_path = os.path.join(
+                *sys.argv[directory_index:filename_index - 1])
 
-        directory_path = os.path.join(
-            *sys.argv[directory_index:filename_index - 1])
+        else:
+            directory_path = os.path.join(
+                *sys.argv[directory_index:])
+
         filename = sys.argv[filename_index]
         os.makedirs(directory_path, exist_ok=True)
 
-        content = []
-        while True:
-            line = input("Enter content line: ")
-            if line.lower() == "stop":
-                break
-            content.append(line.strip())
         file_path = os.path.join(directory_path, filename)
-        create_file(file_path, content)
+        create_file(file_path)
 
-    elif "-f" in sys.argv:
+    elif "-f" in sys.argv and "-d" not in sys.argv:
         filename_index = sys.argv.index("-f") + 1
         filename = sys.argv[filename_index]
 
-        content = []
-        while True:
-            line = input("Enter content line: ")
-            if line.lower() == "stop":
-                break
-            content.append(line.strip())
-
-        create_file(filename, content)
+        create_file(filename)
 
 
 if __name__ == "__main__":
